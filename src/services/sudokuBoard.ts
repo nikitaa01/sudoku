95352b5516dc63b1e84e152486cc0eabeabb9619
nikitaa01 <nikitakuzmenko1107@gmail.com>
2023-10-05 15:58:48 +0200

    fix resolve sudoku endpoint url

--- src/services/sudokuBoard.ts
+++ src/services/sudokuBoard.ts
@@ -2,7 +2,7 @@ import SudokuData from '@/types/sudokuData'
 
 async function getSudokuData(query: URLSearchParams): Promise<SudokuData & { boardStr: string } | undefined> {
     try {
-        const resolveSudokuDataRes = await fetch(`${process.env.API}/resolve-sudoku-board?${query.toString()}`, { cache: 'no-store' })
+        const resolveSudokuDataRes = await fetch(`${process.env.API ?? '/api'}/resolve-sudoku-board?${query.toString()}`, { cache: 'no-store' })
         return await resolveSudokuDataRes.json()
     } catch (error) {
         console.error(error)
