a049241d76ef01c531fa86a7d5ca93b6794dffca
nikitaa01 <nikitakuzmenko1107@gmail.com>
2023-11-16 15:06:31 +0100

    Fix z-index of skeleton

--- src/components/GameSkeleton.tsx
+++ src/components/GameSkeleton.tsx
@@ -6,14 +6,14 @@ export default function GameSkeleton() {
   return (
     <main className="w-screen h-screen flex justify-center items-center">
       <div
-        className="absolute w-full h-full opacity-50 z-10 bg-white"
+        className="absolute w-full h-full opacity-50 z-[1] bg-white"
       />
       <div className="flex flex-col">
         <div className="flex justify-between py-2">
           <Mistakes />
           <Timer />
         </div>
-        <div id="grid-cells" className="z-20 w-[360px] h-[360px] md:w-[432px] md:h-[432px] grid grid-cols-7 grid-rows-7 animate-pulse gap-2">
+        <div id="grid-cells" className="z-[2] w-[360px] h-[360px] md:w-[432px] md:h-[432px] grid grid-cols-7 grid-rows-7 animate-pulse gap-2">
           {new Array(49).fill(null).map(() => (
             <div
               key={Math.random()}
